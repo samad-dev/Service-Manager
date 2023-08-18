8b37de11d9147faf0d1097b362a554e2e85dd178
samad-dev <abdulsamadq67@gmail.com>
2023-08-18 15:26:02 +0500

    footerupdate

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@
                             </div>
                             <div class="col-sm-6">
                                 <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://themesdesign.com/" target="_blank" class="text-reset">Themesdesign</a>
+                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://p2ptack360.com/" target="_blank" class="text-reset">P2P Track 360</a>
                                 </div>
                             </div>
                         </div>
